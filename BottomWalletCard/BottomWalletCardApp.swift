50fab62e4af44a81bcbbc7bd41547e528708bd1a
varsha <varshasahu0402@gmail.com>
2026-02-06 13:14:06 +0530

    Initial Commit

---
+++ BottomWalletCard/BottomWalletCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomWalletCardApp.swift
+//  BottomWalletCard
+//
+//  Created by Varsha Sahu on 06/02/26.
+//
+
+import SwiftUI
+
+@main
+struct BottomWalletCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
